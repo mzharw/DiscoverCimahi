8461dac0c555b6d6fb275f0104bafa4d790194d2
Muhammad Azhari Wirasena <69082763+mzharw@users.noreply.github.com>
2021-06-08 23:15:13 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
-use Illuminate\Support\ServiceProvider;
+  use Illuminate\Support\ServiceProvider;
+  use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
